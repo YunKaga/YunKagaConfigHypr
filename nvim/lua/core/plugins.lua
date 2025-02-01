6ded779646beae4aae11c8facb4eb9603133e59f
YunKaga <voronzowyura@gmail.com>
2025-02-01 17:37:34 +0700

    удалены несколько lsp для nvim

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
-},
+    },
     { 'nvim-treesitter/nvim-treesitter' },
     { 'navarasu/onedark.nvim' },
     {
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 lspconfig.clangd.setup({
     cmd = { "clangd" },
-    filetypes = {"c", "cpp", "objc", "objcpp"},
+    filetypes = {"c", "cpp", "hpp", "h", "objc", "objcpp"},
     single_file_support = true,
     capabilities = capabilities,
 })
@@ -10,11 +10,3 @@ lspconfig.clangd.setup({
 lspconfig.pyright.setup{}
 
 lspconfig.lua_ls.setup({})
-
-lspconfig.emmet_ls.setup{
-    capabilities = capabilities,
-}
-
-lspconfig.ts_ls.setup{
-    capabilities = capabilities,
-}
