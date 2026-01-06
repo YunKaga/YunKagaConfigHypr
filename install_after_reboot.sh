5b03a5c82886e9cd8d4b3c9b58c8fa97fe062ecd
YunKaga <voronzowyura@gmail.com>
2026-01-07 04:21:15 +0700

    da

--- install_after_reboot.sh
+++ install_after_reboot.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Конфиги
+mkdir ~/gitclone
+cd ~/gitclone
+git clone https://github.com/YunKaga/YunKagaConfigHypr
+cd ./YunKagaConfigHypr
+mkdir ~/.config
+rm -r ./README.md ./pockets.txt ./preview
+mv ./* ~/.config/
+mv ./.git ~/.config/
+cd /
+
+systemctl enable sddm
+
 mv ~/.config/.zshrc ~/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
@@ -11,3 +24,5 @@ cd yay
 makepkg -si
 
 yay -Sy spotify zoom
+
+chsh -s /bin/zsh $USER
