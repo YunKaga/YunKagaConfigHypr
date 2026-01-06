6891fc63b4a93488bc0ea63614de0afdbec642ad
YunKaga <voronzowyura@gmail.com>
2026-01-07 04:55:15 +0700

    fix

--- install_after_reboot.sh
+++ install_after_reboot.sh
@@ -2,19 +2,12 @@
 
 # Конфиги
 mkdir ~/.config
-rm -r ./README.md ./pockets.txt ./preview
+rm -r ./README.md ./preview
 mv ./* ~/.config/
 mv ./.git ~/.config/
 cd /
 
-systemctl enable sddm
-
-mv ~/.config/.zshrc ~/
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-source ~/.zshrc
-
-chsh -s /bin/zsh $USER
+sudo systemctl enable sddm
 
 cd ~/gitclone/
 git clone https://aur.archlinux.org/yay.git
@@ -22,3 +15,10 @@ cd yay
 makepkg -si
 
 yay -Sy spotify zoom
+
+rm ~/.zshrc
+mv ~/.config/.zshrc ~/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+source ~/.zshrc
+
