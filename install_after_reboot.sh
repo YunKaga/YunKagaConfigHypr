af182064bdbad8b20557608ab897e1fcff983bd8
YunKaga <voronzowyura@gmail.com>
2026-01-07 21:10:13 +0700

    script

--- install_after_reboot.sh
+++ install_after_reboot.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 setfont cyr-sun16
 
 sudo systemctl enable sddm
 
-mkdir ~/gitclone/
+mkdir ~/gitclone/ 2> /dev/null
 cd ~/gitclone/
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -13,6 +13,9 @@ makepkg -si
 yay -Sy spotify zoom
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 sudo cp -r /usr/share/zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
 sudo cp -r /usr/share/zsh/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+rm ~/.zshrc 2> /dev/null
+cp ~/.config/.zshrc ~/
